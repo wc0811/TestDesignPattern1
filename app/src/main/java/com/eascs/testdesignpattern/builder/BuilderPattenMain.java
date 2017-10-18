17e6d9b5c40e1caaabd884dda06ba99e66a65ced
chao.wang <wc0811@163.com>
2017-10-18 16:30:35 +0800

    the thing i know

--- app/src/main/java/com/eascs/testdesignpattern/builder/BuilderPattenMain.java
+++ app/src/main/java/com/eascs/testdesignpattern/builder/BuilderPattenMain.java
@@ -5,9 +5,12 @@ package com.eascs.testdesignpattern.builder;
  * <p>
  * email：wc0811@163.com
  * <p>
- * 类描述：
+ * 类描述：建造者模式和模版模式，没弄明白具体的不同之处，没了解他们具体的不同
  * <p>
- * 更改记录：
+ * 更改记录：建造者是在模型模式的基础上添加一个方法，这个方法的作用就是来确定每个模型上的方法的执行步骤，就这么简单，
+ * 对，我的理解就是这么简单
+ * 定义：讲一个复杂的构建与他的表示分离，使得同样的构建过程可以创建不同的表示、
+ * 缺点：重点：建造者模式将就的就是顺序，顺序是王
  */
 public class BuilderPattenMain {
     public BuilderPattenMain() {
