838c0ba0d303308d78eba4e30fd81463389165ba
chao.wang <wc0811@163.com>
2017-10-20 15:45:00 +0800

    underStander

---
+++ app/src/main/java/com/eascs/testdesignpattern/command/InVoker.java
@@ -0,0 +1,15 @@
+package com.eascs.testdesignpattern.command;
+
+/**
+ * Create By chao.wang on 2017/10/20 15:07
+ * <p>
+ * email：wc0811@163.com
+ * <p>
+ * 类描述：命令模式的关键，分发命令和执行响应的命令
+ * <p>
+ * 更改记录：用到的词是高内聚，简而言之，就是聚合度特别的搞，
+ * 基本上都在具体内容上进行实现
+ * 定义：将一个请求封装一个对象，从而让你使用不同的请求使客户端参数化
+ */
+public class InVoker {
+}
