2e1d4809f0e3104b41b3c561069c53d32cce277f
chao.wang <wc0811@163.com>
2017-10-23 11:10:11 +0800

    no thinking

--- app/src/main/java/com/eascs/testdesignpattern/chain/Main.java
+++ app/src/main/java/com/eascs/testdesignpattern/chain/Main.java
@@ -8,8 +8,11 @@ import java.util.Random;
  * <p>
  * email：wc0811@163.com
  * <p>
- * 类描述：
- * <p>
+ * 类描述：self-confident//adj
+ * self-confidence//n
+ * passion
+ * chanllenge
+ * <p>责任链模式，各种不想弄了，代码没的写了，非常没啥意思
  * 更改记录：
  */
 public class Main {
