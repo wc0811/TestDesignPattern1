1ed00ff42d8afdfef941d6358baa0fda26bcd140
chao.wang <wc0811@163.com>
2017-10-24 13:35:55 +0800

    new UnderStanding

---
+++ app/src/main/java/com/eascs/testdesignpattern/compose/ComposeMain.java
@@ -0,0 +1,15 @@
+package com.eascs.testdesignpattern.compose;
+
+/**
+ * Create By chao.wang on 2017/10/24 13:34
+ * <p>
+ * email：wc0811@163.com
+ * <p>
+ * 类描述：内容与以前的模式差不多，是一种数据模式，与迭代器模式具有相似的要求
+ * <p>
+ * 更改记录：
+ */
+public class ComposeMain {
+    public ComposeMain() {
+    }
+}
