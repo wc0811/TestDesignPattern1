a55564a4f5f5822954ddb282b98f2274f2b50d01
chao.wang <wc0811@163.com>
2017-10-31 15:08:45 +0800

    visitor

--- app/src/main/java/com/eascs/testdesignpattern/visitor/cmn/Element.java
+++ app/src/main/java/com/eascs/testdesignpattern/visitor/cmn/Element.java
@@ -7,12 +7,12 @@ import com.eascs.testdesignpattern.visitor.IVisitor;
  * <p>
  * email：wc0811@163.com
  * <p>
- * 类描述：
+ * 类描述：优点：1、符合单一原则；2、优秀的扩展性；3、灵活性高；
+ * 缺点：抛弃了对抽象类的依赖，直接对具体类进行操作，扩展困难
  * <p>
  * 更改记录：
  */
 public abstract class Element {
-
     public abstract void doSomething();//定义业务逻辑
 
     public abstract void accept(IVisitor visitor);//允许谁来访问
--- app/src/main/java/com/eascs/testdesignpattern/visitor/cmn/ObjectStrutrue.java
+++ app/src/main/java/com/eascs/testdesignpattern/visitor/cmn/ObjectStrutrue.java
@@ -8,6 +8,7 @@ import java.util.Random;
  * email：wc0811@163.com
  * <p>
  * 类描述：简单的处理方式，一样的完成响应的任务
+ * 通过访问者访问，只要是具体元素就非常容易访问，对元素的遍历就更加容易了，甭管他是什么对象，只要他在一个容器中，都可以通过访问者来访问，任务集中化，这就是访问者模式
  * <p>
  * 更改记录：
  */
@@ -19,6 +20,11 @@ public class ObjectStrutrue {
             new ConcreteElement1();
         } else {
             new ConcreteElement2();
+        }
+
+        for (int i = 0; i < 10; i++) {
+//            Element e1 = new ObjectStrutrue();获取元素对象
+//            e1.accept(new Visitor());接受访问者访问
         }
     }
 }
